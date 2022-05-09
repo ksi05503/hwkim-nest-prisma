a4c7940a1213c48d384259ce6ad30d0b7114f439
hwkim5659 <hwkim5659@cashwalk.io>
2022-05-09 11:26:59 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Prisma Day - NestJS Prisma Workshop')
+    .setTitle('HWKIM Prisma Day - NestJS Prisma Workshop')
     .setDescription('Building a REST API with NestJS and Prisma')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, { customSiteTitle: 'Prisma Day' });
 
-  await app.listen(3000);
+  await app.listen(9009);
 }
 bootstrap();
