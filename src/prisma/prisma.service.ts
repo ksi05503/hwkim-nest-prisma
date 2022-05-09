d29d4aa8e4ac22a8bcf7aa0698c47199c86b6cb3
hwkim5659 <hwkim5659@cashwalk.io>
2022-05-09 11:55:54 +0900

    create PrismaService extends PrismaClient for DI & connect nest logic layer

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({ log: ['info'] }); // 필요한 경우 이곳에서 prisma client option 을 설정할 수 있다.(log level 과 같은)
+  }
+}
